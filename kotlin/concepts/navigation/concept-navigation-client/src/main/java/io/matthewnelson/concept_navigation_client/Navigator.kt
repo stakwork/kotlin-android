9553f34b0a58e4ab7b07d56612ae3f6ad9870032
Matthew Nelson <developer@matthewnelson.io>
2021-02-19 16:51:59 -0500

    Add License Header to all files

--- kotlin/concepts/navigation/concept-navigation-client/src/main/java/io/matthewnelson/concept_navigation_client/Navigator.kt
+++ kotlin/concepts/navigation/concept-navigation-client/src/main/java/io/matthewnelson/concept_navigation_client/Navigator.kt
@@ -1,3 +1,18 @@
+/*
+*  Copyright 2021 Matthew Nelson
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+* */
 package io.matthewnelson.concept_navigation_client
 
 /**
@@ -6,9 +21,6 @@ package io.matthewnelson.concept_navigation_client
  * be completely decoupled while ensuring that arguments are passed if need be (by
  * requiring an argument in the [NavigationRequest]'s constructor, and implementing
  * the passing of said argument via [NavigationRequest.navigate]'s execution.
- *
- * @sample [io.opensolutions.home.navigation.HomeDrawerNavigator]
- * @sample [io.opensolutions.toxicity.navigation.HomeDrawerNavigatorImpl]
  * */
 abstract class Navigator<T>(
     protected val navigationDriver: BaseNavigationDriver<T>
